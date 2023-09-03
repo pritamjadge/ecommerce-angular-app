f464fe922920f5de9c9d96aac5b9c30d7b36ac13
Pritam Jadge <pritam.jadge@gmail.com>
2023-09-03 23:59:50 +0530

    refactor code.

--- src/app/pages/nav-bar/sign-up/sign-up.component.ts
+++ src/app/pages/nav-bar/sign-up/sign-up.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {AuthenticationService} from "../../../services/authentication.service";
-import {ToastrService} from "ngx-toastr";
-import {Router} from "@angular/router";
-import {User} from "../../../models/User";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthenticationService } from "../../../services/authentication.service";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { User } from "../../../models/User";
 
 @Component({
   selector: 'app-sign-up',
@@ -12,46 +12,57 @@ import {User} from "../../../models/User";
 })
 export class SignUpComponent implements OnInit {
 
-  showPassword: string = "password";
   signUpForm!: FormGroup;
 
-  user: User | undefined;
-
-  constructor(private _fb: FormBuilder,
-              private _authService: AuthenticationService,
-              private _toastr: ToastrService,
-              private _router: Router) {
-  }
+  constructor(
+    private _fb: FormBuilder,
+    private _authService: AuthenticationService,
+    private _toastr: ToastrService,
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.signUpForm = this._fb.group({
-      'firstName': ['Pritam', [Validators.required]],
-      'lastName': ['Jadge', [Validators.required]],
-      'emailId': ['pritam.jadge@gmail.com', [Validators.required, Validators.email]],
-      'password': ['Pritam@1994', [Validators.required]],
-      'confirmPassword': ['Pritam@1994', [Validators.required]],
-      'role': [["user"]]
-    })
+    this.initializeForm();
   }
 
-  signUp(formData: any): any {
-    console.log(formData);
-    if (!(formData.password === formData.confirmPassword))
-      return false;
+  initializeForm(): void {
+    this.signUpForm = this._fb.group({
+      firstName: ['Pritam', [Validators.required]],
+      lastName: ['Jadge', [Validators.required]],
+      emailId: ['pritam.jadge@gmail.com', [Validators.required, Validators.email]],
+      password: ['Pritam@1994', [Validators.required]],
+      confirmPassword: ['Pritam@1994', [Validators.required]],
+      role: [['user']]
+    });
+  }
 
-    let username = formData.firstName + " " + formData.lastName;
+  signUp(formData: any): void {
+    if (formData.password !== formData.confirmPassword) {
+      return;
+    }
 
-    this._authService.signUp(new User(username, formData.emailId, formData.password, formData.role)).subscribe({
+    const { firstName, lastName, emailId, password, role } = formData;
+    const username = `${firstName} ${lastName}`;
+    console.log(username);
+    const newUser = new User(username, emailId, password, role);
+
+    this._authService.signUp(newUser).subscribe({
       next: (resp) => {
         console.log(resp);
-        this._router.navigate(['/login']).then();
-        this._toastr.success("User Sign Up Successfully..!");
+        this.navigateToLogin();
+        this.showSuccessMessage("User Sign Up Successfully..!");
       },
       error: (err) => {
         console.log(err);
       }
-    })
+    });
   }
 
+  navigateToLogin(): void {
+    this._router.navigate(['/login']).then();
+  }
 
+  showSuccessMessage(message: string): void {
+    this._toastr.success(message);
+  }
 }
