c2768aae1d9cfb9580c419d464cd07be628de3ad
Pritam Jadge <pritam.jadge@gmail.com>
2023-10-14 15:42:02 +0530

    pagination navigation issue fixed.

--- src/app/pages/view-products/product-list.component.ts
+++ src/app/pages/view-products/product-list.component.ts
@@ -1,42 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {ProductService} from '../../services/product.service';
-import {Product} from '../../models/Product';
-import {HttpErrorResponse} from '@angular/common/http';
-import {MatPaginator} from '@angular/material/paginator';
-import {Category} from '../../models/Category';
-import {AuthenticationService} from "../../services/authentication.service";
+// product-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/Product';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Category } from '../../models/Category';
+import { AuthenticationService } from '../../services/authentication.service';
 import Swal from 'sweetalert2';
-import {Router} from "@angular/router";
-import {CartService} from "../../services/cart.service";
-import {ToastrService} from "ngx-toastr";
-import {BreadcrumbService} from "../../services/breadcrumb.service";
+import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { ToastrService } from 'ngx-toastr';
+import { BreadcrumbService } from '../../services/breadcrumb.service';
+import { MatLegacyPaginator } from '@angular/material/legacy-paginator';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  // Properties
+
   pageIndex = 0;
   pageSize = 12;
   products: Product[] = [];
   categories: Category[] = [];
   isLoading = false;
-  totalPages: number | undefined;
   productName = '';
   categoryId: any = '';
-  productNotFoundMessage: string = "";
-  errorMessage: string = "";
+  productNotFoundMessage: string = '';
+  errorMessage: string = '';
+  totalItems!: number;
 
-
-  constructor(private productService: ProductService,
-              private authService: AuthenticationService,
-              private _router: Router,
-              private cartService: CartService,
-              private _toastr: ToastrService,
-              private breadcrumbService: BreadcrumbService) {
-  }
+  constructor(
+    private productService: ProductService,
+    private authService: AuthenticationService,
+    private _router: Router,
+    private cartService: CartService,
+    private _toastr: ToastrService,
+    private breadcrumbService: BreadcrumbService
+  ) {}
 
   ngOnInit(): void {
     // Initialize data on component load
@@ -49,7 +50,7 @@ export class ProductListComponent implements OnInit {
   // Event Handlers
 
   // Handle category change event
-  getCategoryProductsOnChange(event: any, matPaginator: MatPaginator) {
+  getCategoryProductsOnChange(event: any, matPaginator: MatLegacyPaginator) {
     const pageIndex = matPaginator.pageIndex;
     const pageSize = matPaginator.pageSize;
     this.categoryId = event.target.value;
@@ -57,7 +58,7 @@ export class ProductListComponent implements OnInit {
   }
 
   // Handle product name input change event
-  findByProductsName(event: any, matPaginator: MatPaginator) {
+  findByProductsName(event: any, matPaginator: MatLegacyPaginator) {
     const pageIndex = matPaginator.pageIndex;
     const pageSize = matPaginator.pageSize;
     this.productName = event.target.value;
@@ -81,28 +82,29 @@ export class ProductListComponent implements OnInit {
     }
   }
 
-  // Methods
-
   // Fetch products based on name and category
   fetchProducts(productName: string, pageIndex: number, pageSize: number) {
     const categoryId = this.categoryId;
-    this.productService.findByProductsByNameAndCategory(productName, categoryId, pageIndex, pageSize).subscribe({
-      next: (productData: any) => {
-        if (!productData)
-          this.isLoading = false;
-        else
-          this.handleProductData(productData);
-      },
-      error: () => {
-        this.errorMessage = "Something went wrong, Please try again later.";
-      }
-    });
+    this.productService
+      .findByProductsByNameAndCategory(productName, categoryId, pageIndex, pageSize)
+      .subscribe({
+        next: (productData: any) => {
+          if (!productData)
+            this.isLoading = false;
+          else
+            this.handleProductData(productData);
+        },
+        error: () => {
+          this.errorMessage = "Something went wrong, Please try again later.";
+        }
+      });
   }
 
   // Handle product data from API response
   private handleProductData(productData: any): void {
     if (productData !== null) {
-      this.totalPages = productData.totalPages;
+      console.log(productData);
+      this.totalItems = productData.totalItems;
       this.products = productData.content || [];
 
       if (this.products.length === 0) {
@@ -129,7 +131,6 @@ export class ProductListComponent implements OnInit {
         this.categories = category;
       },
       error: (err: any) => {
-        console.log(err.error);
         this.errorMessage = "Something went wrong, Please try again later.";
         this.isLoading = false;
       }
@@ -142,9 +143,9 @@ export class ProductListComponent implements OnInit {
     this.productService.getAllProducts(pageIndex, pageSize).subscribe({
       next: (productData: any) => {
         this.handleProductData(productData);
+        this.totalItems = productData.totalItems;
       },
       error: (err: HttpErrorResponse) => {
-        console.error('Status:', err.status);
         if (err.status == 0) {
           this.errorMessage = "Something went wrong, Please try again later";
         }
